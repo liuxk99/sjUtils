ef83f9dda8621e0d3ce861360bda4a73be6a7618
Thomas Liu <liuxk99@gmail.com>
2020-04-03 17:45:33 +0800

    [Test]<#NF-feature: AndroidTest>for android library.

--- src/androidTest/java/com/sj4a/utils/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/sj4a/utils/ExampleInstrumentedTest.java
@@ -1,14 +1,15 @@
 package com.sj4a.utils;
 
 import android.content.Context;
+import android.util.Log;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import java.io.IOException;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,11 +18,12 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private final static String TAG = ExampleInstrumentedTest.class.getSimpleName();
     @Test
-    public void useAppContext() {
+    public void testcase_AppContext() throws IOException {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.sj.utils.test", appContext.getPackageName());
+        Log.i(TAG, "pkg: " + appContext.getPackageName());
     }
 }
